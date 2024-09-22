8d39911d39f9d47905d7337bc3b98633fbc14331
OmChillure <omchillure4@gmail.com>
2024-09-22 13:52:08 +0530

    download file

--- src/app/(models)/privatemodel/page.tsx
+++ src/app/(models)/privatemodel/page.tsx
@@ -131,7 +131,7 @@ function PersonalModels() {
                     `${entry.title}.py`
                   )}
                 >
-                  Download
+                  Downloads
                   </Button>
             </Link>
           ))
